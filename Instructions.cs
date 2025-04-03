2cb840e75d6803f144dc626102ce7dc189f7e816
Chris White <cwhit163@vols.utk.edu>
2025-04-03 10:39:56 -0700

    stuff

--- Instructions.cs
+++ Instructions.cs
@@ -1,6 +1,6 @@
 public class Exit : IInstruction {
 	private readonly int _code;
-	public Exit(int code) {
+	public Exit(int code=0) {
 		_code = code;
 	}
 	public int Encode() {
@@ -10,24 +10,24 @@ public class Exit : IInstruction {
 
 public class Swap : IInstruction {
 	private readonly int _from, _to;
-	public Swap(fro=4, to=0) {
+	public Swap(int fro=4, int to=0) {
 		_from = fro;
 		_to = to;
 	}
 	public int Encode() {
-		return (0b00000001 << 24) | (_from << 12) | _to
+		return (0b00000001 << 24) | (_from << 12) | _to;
 	}
 }
 
 public class Nop : IInstruction {
-	public Nop() {};
+	public Nop() {}
 	public int Encode() {
 		return 0b0010 << 24;
 	}
 }
 
 public class Input : IInstruction {
-	public Input() {};
+	public Input() {}
 	public int Encode() {
 		return 0b0100 << 24;
 	}
@@ -36,7 +36,7 @@ public class Input : IInstruction {
 public class Stinput : IInstruction {
 	private readonly int _UMC;
 	public Stinput(int UMC = 0x00FFFFFF) {
-		_UMC = UMC;
+		_UMC = 0xFFFFFF & UMC;
 	}
 	public int Encode() {
 		return (0b0101 << 24) | _UMC;
@@ -63,12 +63,300 @@ public class Pop : IInstruction {
 	}
 }
 
+public class Add : IInstruction {
+	public Add() {}
+	public int Encode() {
+		return 0b00100000 << 24;
+	}
+}
+
+public class Sub : IInstruction {
+	public Sub() {}
+	public int Encode() {
+		return 0b00100001 << 24;
+	}
+}
+
+public class Mul : IInstruction {
+	public Mul() {}
+	public int Encode() {
+		return 0b00100010 << 24;
+	}
+}
+
+public class Div : IInstruction {
+	public Div() {}
+	public int Encode() {
+		return 0b00100011 << 24;
+	}
+}
+
+public class Rem : IInstruction {
+	public Rem() {}
+	public int Encode() {
+		return 0b00100100 << 24;
+	}
+}
+
+public class And : IInstruction {
+	public And() {}
+	public int Encode() {
+		return 0b00100101 << 24;
+	}
+}
+
+public class Or : IInstruction {
+	public Or() {}
+	public int Encode() {
+		return 0b00100110 << 24;
+	}
+}
+
+public class Xor : IInstruction {
+	public Xor() {}
+	public int Encode() {
+		return 0b00100111 << 24;
+	}
+}
+
+public class Lsl : IInstruction {
+	public Lsl() {}
+	public int Encode() {
+		return 0b00101000 << 24;
+	}
+}
+
+public class Lsr : IInstruction {
+	public Lsr() {}
+	public int Encode() {
+		return 0b00101001 << 24;
+	}
+}
+
+public class Asr : IInstruction {
+	public Asr() {}
+	public int Encode() {
+		return 0b00101011 << 24;
+	}
+}
+
+public class Neg : IInstruction {
+	public Neg() {}
+	public int Encode() {
+		return 0b00110000 << 24;
+	}
+}
+
+public class Not : IInstruction {
+	public Not() {}
+	public int Encode() {
+		return 0b00110001 << 24;
+	}
+}
+
+public class Stprint : IInstruction {
+	private readonly int _offset;
+	public Stprint(int offset=0) {
+		_offset = offset & ~3;
+	}
+	public int Encode() {
+		return (0b0100 << 28) | _offset;
+	}
+}
+
+public class Call : IInstruction {
+	private readonly int _offset;
+	public Call(int offset) {
+		_offset = offset & ~3;
+	}
+	public int Encode() {
+		return (0b0101 << 28) | _offset;
+	}
+}
+
+public class Return : IInstruction {
+	private readonly int _offset;
+	public Return(int offset=0) {
+		_offset = offset & ~3;
+	}
+	public int Encode() {
+		return (0b0110 << 28) | _offset;
+	}
+}
+
+public class Goto : IInstruction {
+	public readonly int _offset;
+	public Goto(int offset) {
+		_offset = offset & ~3;
+	}
+	public int Encode() {
+		return (0b0111 << 28) | _offset;
+	}
+}
+
+public class Ifeq : IInstruction {
+	public readonly int _offset;
+	public Ifeq(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000000 << 25) | _offset;
+	}
+}
+
+public class Ifne : IInstruction {
+	public readonly int _offset;
+	public Ifne(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000001 << 25) | _offset;
+	}
+}
+
+public class Iflt : IInstruction {
+	public readonly int _offset;
+	public Iflt(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000010 << 25) | _offset;
+	}
+}
+
+public class Ifgt : IInstruction {
+	public readonly int _offset;
+	public Ifgt(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000011 << 25) | _offset;
+	}
+}
+
+public class Ifle : IInstruction {
+	public readonly int _offset;
+	public Ifle(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000100 << 25) | _offset;
+	}
+}
+
+public class Ifge : IInstruction {
+	public readonly int _offset;
+	public Ifge(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b1000101 << 25) | _offset;
+	}
+}
+
+public class Ifez : IInstruction {
+	public readonly int _offset;
+	public Ifez(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b100100 << 26) | _offset;
+	}
+}
+
+public class Ifnz : IInstruction {
+	public readonly int _offset;
+	public Ifnz(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b100101 << 26) | _offset;
+	}
+}
+
+public class Ifmi : IInstruction {
+	public readonly int _offset;
+	public Ifmi(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b100110 << 26) | _offset;
+	}
+}
+
+public class Ifpl : IInstruction {
+	public readonly int _offset;
+	public Ifpl(int offset) {
+		_offset = offset & ~ 3;
+	}
+	public int Encode() {
+		return (0b100111 << 26) | _offset;
+	}
+}
+
 public class Dup : IInstruction {
     private readonly int _offset;
-    public Dup(int offset) {
+    public Dup(int offset=0) {
         _offset = offset & ~3;
     }
     public int Encode() {
         return (0b1100 << 28) | _offset;
     }
+}
+
+public class Print : IInstruction {
+	private readonly int _offset;
+	public Print(int offset=0) {
+		_offset = offset;
+	}
+	public int Encode() {
+		return (0b1101 << 28) | _offset << 2 | 00;
+	}
+}
+
+public class Printh : IInstruction {
+	private readonly int _offset;
+	public Printh(int offset=0) {
+		_offset = offset;
+	}
+	public int Encode() {
+		return (0b1101 << 28) | _offset << 2 | 01;
+	}
+}
+
+public class Printb : IInstruction {
+	private readonly int _offset;
+	public Printb(int offset=0) {
+		_offset = offset;
+	}
+	public int Encode() {
+		return (0b1101 << 28) | _offset << 2 | 10;
+	}
+}
+
+public class Printo : IInstruction {
+	private readonly int _offset;
+	public Printo(int offset=0) {
+		_offset = offset;
+	}
+	public int Encode() {
+		return (0b1101 << 28) | _offset << 2 | 11;
+	}
+}
+
+public class Dump : IInstruction {
+	public Dump() {}
+	public int Encode() {
+		return 0b1110 << 28;
+	}
+}
+
+public class Push: IInstruction {
+	private readonly int _offset;
+	public Push(int offset=0) {
+		_offset = offset;
+	}
+	public int Encode() {
+		return (0b1111 << 28) | _offset;
+	}
 }
--- Program.cs
+++ Program.cs
@@ -47,7 +47,6 @@ class Pass1 {
 				pushst = pushst.Replace("\\\\", "\\");
 				pushst = pushst.Replace("\\\"", "\"");
 				pushst = pushst.Replace("\\\n", "|");
-				Console.WriteLine(pushst);
 
 				buf = "";
 				for (int i = 0; i < pushst.Length; i++) {
@@ -76,6 +75,10 @@ class Pass1 {
 					memLoc += 4;
 				} 
 				hex.Clear();
+
+				memory.Add(memLoc, new string[] {"stprint"});
+
+				memLoc += 4;
 			}
 
 			lineNo++;
@@ -92,6 +95,140 @@ class Pass1 {
 
 		foreach (var entry in labels) {
 			Console.WriteLine($"{entry.Key[0]}: {entry.Value} {entry.Key[1]}");
+		}
+
+		WriteBytes(memory, labels);
+	}
+
+	public static void WriteBytes(Dictionary<int, string[]> memory, Dictionary<int[], string> labels) {
+		using (BinaryWriter writer = new BinaryWriter(File.Open("tmp.txt", FileMode.Create))) {
+			foreach (var entry in memory) {
+				string cmd = entry.Value[0];
+
+				if (cmd == "exit") {
+					Exit exit;
+					try {
+						exit = new Exit(int.Parse(entry.Value[1]));
+					} catch {
+						exit = new Exit();
+					}
+				
+            		writer.Write(exit.Encode());
+        		} else if (cmd == "swap") {
+					Swap swap;
+					try {
+						swap = new Swap(int.Parse(entry.Value[1]), int.Parse(entry.Value[2]));
+					} catch {
+						try {
+						swap = new Swap(int.Parse(entry.Value[1]));
+						} catch {
+							swap = new Swap();
+						}
+					}
+					writer.Write(swap.Encode());
+				} else if (cmd == "nop") {
+					Nop nop = new Nop();
+					writer.Write(nop.Encode());
+				} else if (cmd == "input") {
+					Input inp = new Input();
+					writer.Write(inp.Encode());
+				} else if (cmd == "stinput") {
+					Stinput stinput;
+					if (entry.Value.Length == 2) {
+						try {
+							stinput = new Stinput(int.Parse(entry.Value[1]));
+						} catch {
+							stinput = new Stinput(int.Parse(entry.Value[1].Substring(2), System.Globalization.NumberStyles.HexNumber));
+						}
+					} else {
+						stinput = new Stinput();
+					}
+					writer.Write(stinput.Encode());
+				} else if (cmd == "debug") {
+					Debug debug;
+					if (entry.Value.Length == 2) {
+						try {
+							debug = new Debug(int.Parse(entry.Value[1]));
+						} catch {
+							debug = new Debug(int.Parse(entry.Value[1].Substring(2), System.Globalization.NumberStyles.HexNumber));
+						}
+					} else {
+						debug = new Debug();
+					}
+					writer.Write(debug.Encode());
+				} else if (cmd == "pop") {
+					Pop pop;
+					if (entry.Value.Length == 2) {
+						pop = new Pop(int.Parse(entry.Value[1]));
+					} else {
+						pop = new Pop();
+					}
+					writer.Write(pop.Encode());
+				} else if (cmd == "add") {
+					Add add = new Add();
+					writer.Write(add.Encode());
+				} else if (cmd == "sub") {
+					Sub sub = new Sub();
+					writer.Write(sub.Encode());
+				} else if (cmd == "mul") {
+					Mul mul = new Mul();
+					writer.Write(mul.Encode());
+				} else if (cmd == "div") {
+					Div div  = new Div();
+					writer.Write(div.Encode());
+				} else if (cmd == "rem") {
+					Rem rem = new Rem();
+					writer.Write(rem.Encode());
+				} else if (cmd == "and") {
+					And and = new And();
+					writer.Write(and.Encode());
+				} else if (cmd == "or") {
+					Or or = new Or();
+					writer.Write(or.Encode());
+				} else if (cmd == "xor") {
+					Xor xor = new Xor();
+					writer.Write(xor.Encode());
+				} else if (cmd == "lsl") {
+					Lsl lsl = new Lsl();
+					writer.Write(lsl.Encode());
+				} else if (cmd == "lsr") {
+					Lsr lsr = new Lsr();
+					writer.Write(lsr.Encode());
+				} else if (cmd == "asr") {
+					Asr asr = new Asr();
+					writer.Write(asr.Encode());
+				} else if (cmd == "neg") {
+					Neg neg = new Neg();
+					writer.Write(neg.Encode());
+				} else if (cmd == "not") {
+					Not not = new Not();
+					writer.Write(not.Encode());
+				} else if (cmd == "stprint") {
+					Stprint stprint;
+					if (entry.Value.Length == 2) {
+						try {
+							stprint = new Stprint(int.Parse(entry.Value[1]));
+						} catch {
+							stprint = new Stprint(int.Parse(entry.Value[1].Substring(2), System.Globalization.NumberStyles.HexNumber));
+						}
+					} else {
+						stprint = new Stprint();
+					}
+					writer.Write(stprint.Encode());
+				} else if (cmd == "call") {
+					int loc = labels.FirstOrDefault(x => x.Value == entry.Value[1]).Key[0];
+					Call call = new Call(loc);
+					writer.Write(call.Encode());
+				} else if (cmd == "return") {
+					Return ret;
+					if (entry.Value.Length == 2) {
+						ret = new Return(int.Parse(entry.Value[1]));
+					} else {
+						ret = new Return();
+					}
+					writer.Write(ret.Encode());
+				}
+			}
 		}
 	}
 }
